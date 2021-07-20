9df5f45e2594d879863f145989f16413a8653756
kod <username@alpha-trion.myfiosgateway.com>
2021-07-20 06:19:25 -0400

    ...

--- behavior.js
+++ behavior.js
@@ -1,20 +1,13 @@
 const behaviors = require('./lib/bh_core.js');
-
 var countSystemSleep = 0;
-
 
 (async () => {
 await behaviors.splash();
 await behaviors.init();
-
 do{
-
-// this needs its own function
-// go thru each command ...
-await behaviors.cycle();
+	// go thru each command 
+	await behaviors.cycle();
 }while(true)
 
 process.exit();
-
-
 })();
